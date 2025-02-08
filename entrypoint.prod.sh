c22f9f44274f6c06da48c2bfff0c36bb4ca1386d
David <david@mac.home>
2025-02-07 22:55:19 -0500

    entrypoint changed

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -2,4 +2,4 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-python -m gunicorn --bind 0.0.0.0:$PORT --workers 3 ca_tips_forum.wsgi:application
+python -m gunicorn --bind 0.0.0.0:$PORT --workers 1 ca_tips_forum.wsgi:application
